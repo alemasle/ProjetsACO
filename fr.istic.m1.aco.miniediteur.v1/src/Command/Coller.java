5c2ff373b4f6215e832c85b178439b637174035a
Papanevada <alexislm35@hotmail.fr>
2017-10-21 21:55:05 +0200

    reup

--- fr.istic.m1.aco.miniediteur.v1/src/Command/Coller.java
+++ fr.istic.m1.aco.miniediteur.v1/src/Command/Coller.java
@@ -5,72 +5,72 @@ import Receiver.*;
 
 /**
  * Concrete Command "Coller" implementant l'interface Command.
- * 
+ *
  * @author Alexis LE MASLE et Fanny PRIEUR
- * 
  */
 public class Coller implements Command {
 
-	/**
-	 * Nouvelle instance de l'interface Moteur declarant la methode coller
-	 * 
-	 * @see Moteur
-	 */
-	private Moteur moteur;
+    /**
+     * Nouvelle instance de l'interface Moteur declarant la methode coller
+     *
+     * @see Moteur
+     */
+    private Moteur moteur;
 
-	private Enregistreur enregistreur;
+    private Enregistreur enregistreur;
 
-	/**
-	 * Nouveau memento
-	 */
-	private Memento<CollerMemento> memento;
+    /**
+     * Nouveau memento
+     */
+    private Memento<CollerMemento> memento;
 
-	/**
-	 * Constructeur de la classe Coller
-	 * 
-	 * @param moteur
-	 */
-	public Coller(Moteur moteur, Enregistreur enregistreur) {
-		this.moteur = moteur;
-		this.memento = null;
-		this.enregistreur = enregistreur;
-	}
+    /**
+     * Constructeur de la classe Coller
+     *
+     * @param moteur
+     */
+    public Coller(Moteur moteur, Enregistreur enregistreur) {
+        this.moteur = moteur;
+        this.memento = null;
+        this.enregistreur = enregistreur;
+    }
 
-	// Operations
+    // Operations
 
-	/**
-	 * Appel de la mise en oeuvre de la fonction "coller" dans l'implementation
-	 * Moteur.
-	 * 
-	 * @see MoteurImpl
-	 * 
-	 */
-	public void execute() {
-		moteur.coller();
-	}
+    /**
+     * Appel de la mise en oeuvre de la fonction "coller" dans l'implementation
+     * Moteur.
+     *
+     * @see MoteurImpl
+     */
+    public void execute() {
+        moteur.coller();
+    }
 
-	/**
-	 * @return memento le memento cournat
-	 */
-	public Memento getMemento() {
-		return memento;
-	}
+    /**
+     * @return memento le memento cournat
+     */
+    public Memento getMemento() {
+        return memento;
+    }
 
-	/**
-	 * met a jour le Memento courant
-	 */
-	public void setMemento(Memento m) {
-		this.memento = m;
-	}
+    /**
+     * met a jour le Memento courant
+     */
+    public void setMemento(Memento m) {
+        this.memento = m;
+    }
 
-	/**
-	 * Classe privee CollerMemento implementant Memento
-	 * 
-	 * @author Alexis LE MASLE et Fanny PRIEUR
-	 *
-	 */
-	private class CollerMemento implements Memento {
+    /**
+     * Classe privee CollerMemento implementant Memento
+     *
+     * @author Alexis LE MASLE et Fanny PRIEUR
+     */
+    private class CollerMemento implements Memento {
 
-	}
+        Command memCommand = new Coller(moteur, enregistreur);
+
+
+    }
 
 }
--- fr.istic.m1.aco.miniediteur.v1/src/Receiver/EnregistrerImpl.java
+++ fr.istic.m1.aco.miniediteur.v1/src/Receiver/EnregistrerImpl.java
@@ -8,56 +8,54 @@ import Memento.Memento;
 /**
  * Classe {@link EnregistrerImpl} implementant l'interface {@link Enregistreur}
  * contenant une implementation des methodes abstraite de l'interface
- * 
- * @author Alexis LE MASLE et Fanny PRIEUR
  *
+ * @author Alexis LE MASLE et Fanny PRIEUR
  */
 public class EnregistrerImpl implements Enregistreur {
 
-	private Boolean record = false;
+    private Boolean record = false;
 
-	private List<Memento> replay = new ArrayList<Memento>();
+    private List<Memento> replay = new ArrayList<Memento>();
 
-	public EnregistrerImpl() {
+    public EnregistrerImpl() {
 
-	}
+    }
 
-	/**
-	 * Demarre l'enregistrement
-	 */
-	public void demarrer() {
-		setRecord(true);
-	}
+    /**
+     * Demarre l'enregistrement
+     */
+    public void demarrer() {
+        setRecord(true);
+    }
 
-	/**
-	 * Stoppe l'enregistrement en cours
-	 */
-	public void stopper() {
-		setRecord(false);
-	}
+    /**
+     * Stoppe l'enregistrement en cours
+     */
+    public void stopper() {
+        setRecord(false);
+    }
 
-	/**
-	 * Rejoue le dernier enregistrement
-	 */
-	public void rejouer() {
-		for (Memento memento : replay) {
+    /**
+     * Rejoue le dernier enregistrement
+     */
+    public void rejouer() {
+        for (Memento memento : replay) {
 
-		}
-	}
+        }
+    }
 
-	/**
-	 * @return le record
-	 */
-	public Boolean getRecord() {
-		return record;
-	}
+    /**
+     * @return le record
+     */
+    public Boolean getRecord() {
+        return record;
+    }
 
-	/**
-	 * @param record
-	 *            le record à définir
-	 */
-	public void setRecord(Boolean record) {
-		this.record = record;
-	}
+    /**
+     * @param record le record à définir
+     */
+    public void setRecord(Boolean record) {
+        this.record = record;
+    }
 
 }
