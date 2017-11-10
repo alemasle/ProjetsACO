ea5f2fd137a1f9bb45ee76f335d3ce52e642d22a
16000024 <16000024@e103m09.istic.univ-rennes1.fr>
2017-11-10 18:01:09 +0100

    Je sais plus

--- fr.istic.m1.aco.miniediteur.v1/src/Receiver/MoteurImpl.java
+++ fr.istic.m1.aco.miniediteur.v1/src/Receiver/MoteurImpl.java
@@ -43,8 +43,7 @@ public class MoteurImpl implements Moteur {
 	 * Permet de copier la selection.
 	 */
 	public void copier() {
-		clip.setClip(buffer.getBuffer().substring(select.getDebut(),
-				select.getFin()));
+		clip.setClip(buffer.getBuffer().substring(select.getDebut(), select.getFin()));
 	}
 
 	/**
@@ -55,6 +54,7 @@ public class MoteurImpl implements Moteur {
 	 */
 	public void inserer(String str) {
 		// TODO
+		
 	}
 
 	/**
@@ -62,8 +62,7 @@ public class MoteurImpl implements Moteur {
 	 * texte.
 	 */
 	public void couper() {
-		clip.setClip(buffer.getBuffer().substring(select.getDebut(),
-				select.getFin()));
+		clip.setClip(buffer.getBuffer().substring(select.getDebut(), select.getFin()));
 		buffer.getBuffer().delete(select.getDebut(), select.getFin());
 	}
 
