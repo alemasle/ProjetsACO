8b0c3e3a8162ee8ac803b705033b56f81ef3b24f
FangKruger <31961887+FangKruger@users.noreply.github.com>
2017-11-27 21:30:01 +0100

    cscv

---
+++ fr.istic.m1.aco.miniediteur.v3/test/receiverTest/JDelete.java
@@ -0,0 +1,103 @@
+package receiverTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import command.Delete;
+import command.Rejouer;
+import receiver.EnregistrerImpl;
+import receiver.Enregistreur;
+import receiver.Moteur;
+import receiver.MoteurImpl;
+import state.Buffer;
+import state.ClipBoard;
+import state.ClipboardImpl;
+import state.Selection;
+
+public class JDelete {
+
+	@Test
+	public void testJDelete1() {
+
+		StringBuffer stringBuffer = new StringBuffer("delete");
+		Buffer buffer = new Buffer();
+		Selection selection = new Selection();
+		ClipBoard pressePapier = new ClipboardImpl();
+		Enregistreur enregistreur = new EnregistrerImpl();
+		Rejouer rejouer = new Rejouer(enregistreur);
+		Moteur moteur = new MoteurImpl(buffer, pressePapier, selection);
+		Delete delete = new Delete(moteur, enregistreur);
+
+		buffer.setBuffer(stringBuffer);
+
+		selection.setDebut(6);
+		selection.setFin(6);
+
+		enregistreur.demarrer();
+		delete.execute();
+		enregistreur.stopper();
+
+		selection.setDebut(2);
+		selection.setFin(3);
+
+		rejouer.execute();
+		assertTrue(("deet").compareTo(buffer.getBuffer().toString()) == 0);
+	}
+
+	@Test
+	public void testJDelete2() {
+
+		StringBuffer stringBuffer = new StringBuffer("delete");
+		Buffer buffer = new Buffer();
+		Selection selection = new Selection();
+		ClipBoard pressePapier = new ClipboardImpl();
+		Enregistreur enregistreur = new EnregistrerImpl();
+		Rejouer rejouer = new Rejouer(enregistreur);
+		Moteur moteur = new MoteurImpl(buffer, pressePapier, selection);
+		Delete delete = new Delete(moteur, enregistreur);
+
+		buffer.setBuffer(stringBuffer);
+		selection.setDebut(0);
+		selection.setFin(2);
+
+		selection.setDebut(6);
+		selection.setFin(6);
+
+		enregistreur.demarrer();
+
+		delete.execute();
+
+		enregistreur.stopper();
+
+		rejouer.execute();
+		rejouer.execute();
+		rejouer.execute();
+
+		assertTrue(("de").compareTo(buffer.getBuffer().toString()) == 0);
+	}
+
+	@Test
+	public void testJDelete3() {
+
+		StringBuffer stringBuffer = new StringBuffer("deleteDELETE");
+		Buffer buffer = new Buffer();
+		Selection selection = new Selection();
+		ClipBoard pressePapier = new ClipboardImpl();
+		Enregistreur enregistreur = new EnregistrerImpl();
+		Moteur moteur = new MoteurImpl(buffer, pressePapier, selection);
+		Delete delete = new Delete(moteur, enregistreur);
+
+		buffer.setBuffer(stringBuffer);
+
+		selection.setDebut(4);
+		selection.setFin(7);
+		delete.execute();
+		selection.setDebut(4);
+		selection.setFin(7);
+		delete.execute();
+
+		assertTrue(("deleTE").compareTo(buffer.getBuffer().toString()) == 0);
+	}
+
+}
