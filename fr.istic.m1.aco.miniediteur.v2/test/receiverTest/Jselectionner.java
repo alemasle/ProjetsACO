7be125f2d09a4219cfc4e5d524867af919d453ba
alemasle <alexislm35@hotmail.fr>
2017-11-27 19:09:16 +0100

    suppression import inutiles

--- fr.istic.m1.aco.miniediteur.v2/test/receiverTest/Jselectionner.java
+++ fr.istic.m1.aco.miniediteur.v2/test/receiverTest/Jselectionner.java
@@ -1,7 +1,5 @@
 package receiverTest;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.Random;
